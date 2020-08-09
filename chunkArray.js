53cc71b0df05206d611bbeef76ac4b8bc1f7f67b
Zachary Olpin <info@zacharyolp.in>
2020-08-09 13:09:32 -0600

    chunk array

---
+++ chunkArray.js
@@ -0,0 +1,12 @@
+// Chunk an array.
+// chunk([4, 1, 2, 3, 1], 2) => [[4, 1], [2, 3], [1]]
+
+const chunk = (array, chunkSize) => {
+  let arr = []
+  for (let i = 0; i < array.length; i++) {
+    if (i % chunkSize === 0) arr.push([array[i]])
+    else arr[arr.length - 1].push(array[i])
+  }
+  return arr
+}
+
